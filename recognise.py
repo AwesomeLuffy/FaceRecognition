60e966ea2a07c62806daf9694e30b89bbbf74453
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-01-31 13:56:25 -0500

    Change code to matching with Dataset class

--- recognise.py
+++ recognise.py
@@ -1,44 +1,34 @@
+import os
+
 import face_recognition
 from PIL import Image, ImageDraw
+import numpy as np
+
+from dataset import Dataset
 
 
 class Faces:
+    LAST_UNKNOWN_NUMBER = 0
+
     def __init__(self, image_path):
-        self.known_faces = {}
-        self.known_faces_encoded = {}
-        self.load_known_faces()
+        Dataset.load_file()
+
         self.draw = None
         # Convert the image to ndarray
         self.image_to_recognize = face_recognition.load_image_file(image_path)
 
-    # TODO This method will be changed to load the dataset from a file
-    def load_known_faces(self):
+        for key in Dataset.known_faces.keys():
+            if key.startswith("Unknown_"):
+                Faces.LAST_UNKNOWN_NUMBER = int((key.split("_")[1])[:-3])
 
-        yolan_image = face_recognition.load_image_file("yolan.png")
-        benjamin_image = face_recognition.load_image_file("benjamin.png")
-
-        # Here we get the face encoding of the person we want to add to the dataset
-        yolan_encoding = face_recognition.face_encodings(yolan_image)[0]
-        benjamin_encoding = face_recognition.face_encodings(benjamin_image)[0]
-
-        print(yolan_encoding)
-        self.known_faces = {
-            str(yolan_encoding): "yolan",
-            str(benjamin_encoding): "benjamin"
-        }
-
-        self.known_faces_encoded = [yolan_encoding, benjamin_encoding]
-
-    def add_known_faces(self, name, encoding):
-        self.known_faces[name] = encoding
-
-    def add_face_from_image(self, image_path, name):
+    @staticmethod
+    def add_face_from_image(image_path, code: str):
         # Here we load the image of the person we want to add to the dataset
         image = face_recognition.load_image_file(image_path)
         # Here we get the face encoding of the person we want to add to the dataset
         encoding = face_recognition.face_encodings(image)[0]
         # Here we add the face encoding of the person to the array of known faces
-        self.known_faces[name] = encoding
+        Dataset.save_face(code, encoding)
 
     def recognize(self, output: bool = True):
         # Here we check the face and print in a new image the name of the person if there is a match, else it prints
@@ -50,37 +40,52 @@ class Faces:
 
         # Data from the image we want to recognize
         face_locations_image_to_recognize = face_recognition.face_locations(self.image_to_recognize)
-        face_encodings_image_to_recognize = face_recognition.face_encodings(self.image_to_recognize, face_locations_image_to_recognize)
+        face_encodings_image_to_recognize = face_recognition.face_encodings(self.image_to_recognize,
+                                                                            face_locations_image_to_recognize)
 
-        for (top, right, bottom, left), face_encoding in zip(face_locations_image_to_recognize, face_encodings_image_to_recognize):
+        for (top, right, bottom, left), face_encoding in zip(face_locations_image_to_recognize,
+                                                             face_encodings_image_to_recognize):
             # Check if there is a match (first the face we know and then the face we want to recognize)
-            matches = face_recognition.compare_faces(self.known_faces_encoded, face_encoding)
+            matches = face_recognition.compare_faces(list(Dataset.known_faces.values()), face_encoding)
             name = "Unknown Person"
-
             if True in matches:
-                # This line reconvert the actual ndarray to check in the dict what's the name of the person
-                # Use 2 list like before is not 100% sure and we have to know exactly what we do
-                # In this way it's more difficult to have a problem
-
-                # PS : This is the solution used because ndarray are unhashable and convert a string to ndarray
-                # is painfull for nothing
-                name = self.known_faces[str(self.known_faces_encoded[matches.index(True)])]
+                # Get the name of the person
+                # Use the static method "get_key_from_value" to get the name of the person
+                name = Faces.get_key_from_value(list(Dataset.known_faces.values())[matches.index(True)])
+                if output:
+                    Faces.output_result(pil_image, False, name)
+            else:
+                self.LAST_UNKNOWN_NUMBER += 1
+                Dataset.save_face(f"Unknown_{Faces.LAST_UNKNOWN_NUMBER:03d}", face_encoding)
+                if output:
+                    Faces.output_result(pil_image, True)
 
             self.draw_rectangle_around_face(left, right, bottom, top, name)
 
-        if output:
-            pil_image.save("output.jpg")
+            if output:
+                pil_image.save("default_output.jpg")
+
+    @staticmethod
+    def get_key_from_value(value):
+        for key, val in Dataset.known_faces.items():
+            if np.array_equiv(val, value):
+                return key
+        return "None"
+
+    @staticmethod
+    def output_result(pil_image, is_unknown: bool = False, name: str = ""):
+        if is_unknown:
+            output_file_name = f"Unknown_{Faces.LAST_UNKNOWN_NUMBER:03d}.jpg"
+        else:
+            output_file_name = f"{name}.jpg"
+        if output_file_name not in os.scandir("OutputFaces"):
+            pil_image.save(f"OutputFaces/{output_file_name}")
 
     @staticmethod
     def encode_face_from_image(image_path):
         image = face_recognition.load_image_file(image_path)
         encoding = face_recognition.face_encodings(image)[0]
         return encoding
-
-    # TODO The method have to write in the dataset a dict like "name" and "encoding"
-    # def create_dataset(self, dataset_path):
-    #     with open(dataset_path, "wb") as f:
-    #         pickle.dump(..., f)
 
     def draw_rectangle_around_face(self, left, right, bottom, top, name):
         self.draw.rectangle(((left + 2, top + 2), (right + 2, bottom + 2)), outline=(0, 255, 255))
@@ -90,6 +95,6 @@ class Faces:
 
 
 if __name__ == "__main__":
-    face = Faces("yolan.png")
+    face = Faces("test.png")
 
     face.recognize()
