b3cd5ede11de0f3cf559a8bede7ce204e0426dfa
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-22 16:15:26 -0500

    Copied database_handler.py from Flask here (adding update and delete queries)

--- src/Database/database_handler.py
+++ src/Database/database_handler.py
@@ -7,7 +7,7 @@ class DatabaseHandler:
         "port": 3306,
         "user": "root",
         "password": "test",
-        "database": "data_faces",
+        "database": "data_faces"
     }
 
     # Return a connection to the database but not close it
@@ -45,7 +45,12 @@ class DatabaseHandler:
     # Connexion can be passed as parameter to not open another one
     # (will not be closed after, the method will act as subquery)
     @staticmethod
-    def check_value_exists(table_name: str, column_name: str, value: tuple, connexion=None):
+    def check_value_exists(table_name: str, column_name: str, value: tuple, connexion=None) -> bool:
+        """Method to check if value exists in a table
+        Connexion can be passed as parameter to not open another one
+        (will not be closed after, the method will act as subquery)
+        :return: True if value exists or if error happened, False otherwise
+        """
         conn = connexion if connexion is not None else DatabaseHandler.get_conn()
         cursor = conn.cursor()
         SQL_RAW_QUERY = f"SELECT {column_name} FROM {table_name} WHERE {column_name} = %s"
@@ -62,19 +67,53 @@ class DatabaseHandler:
                 conn.close()
 
     @staticmethod
-    def read_values(sql: str, where=None) -> list[tuple]:
+    def read_values(sql: str, where=None, as_dict: bool = False, only_one: bool = False) -> list[tuple]:
         conn = DatabaseHandler.get_conn()
-        cursor = conn.cursor()
+        cursor = conn.cursor(dictionary=as_dict)
         try:
             if where is not None:
                 cursor.execute(sql, where)
             else:
                 cursor.execute(sql)
 
-            return cursor.fetchall()
+            return cursor.fetchall() if not only_one else cursor.fetchone()
         except Exception as e:
             print(e)
             return []
         finally:
             cursor.close()
             conn.close()
+
+    @staticmethod
+    def update_values(sql: str, val: tuple) -> int:
+        conn = DatabaseHandler.get_conn()
+        cursor = conn.cursor()
+        try:
+            cursor.execute(sql, val)
+            conn.commit()
+
+            return cursor.rowcount
+        except Exception as e:
+            print(e)
+            return -1
+        finally:
+            cursor.close()
+            conn.close()
+
+    @staticmethod
+    def delete_values(sql: str, where: tuple | None = None) -> int:
+        conn = DatabaseHandler.get_conn()
+        cursor = conn.cursor()
+        try:
+            cursor.execute(sql) if where is None else cursor.execute(sql, where)
+            conn.commit()
+
+            return cursor.rowcount
+        except Exception as e:
+            print(e)
+            return -1
+        finally:
+            cursor.close()
+            conn.close()
+
+
