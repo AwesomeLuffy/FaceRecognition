3c583832c84374392f35412a2056aaca03ed0319
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-02-21 23:53:31 -0500

    Moving class call to main.py

--- src/recognise_cv2.py
+++ src/recognise_cv2.py
@@ -1,3 +1,5 @@
+import time
+
 import face_recognition
 import cv2
 import numpy as np
@@ -8,11 +10,15 @@ from Utils import get_key_from_value
 from dataset import Dataset
 from recognise import Faces
 from Logs import Logs
+import threading
 
 
 class VideoFR:
+    """
+    VideoFR (Video Face Recognition) is a class that will be used to recognize faces in a video
+    """
     # Font
-    FONT = ImageFont.truetype("./ressources/fonts/Arial.ttf", 10)
+    FONT = ImageFont.truetype("Arial.ttf", 10)
     # fontScale
     font_scale = 1
     # Cooldown Intruder
@@ -22,7 +28,8 @@ class VideoFR:
     # Output picture extension
     EXTENSION = ".jpg"
 
-    def __init__(self):
+    def __init__(self, debug_mode: bool = False):
+        self.debug_mode = debug_mode
         # Face array data
         self.left_face_array = []
         self.right_face_array = []
@@ -35,8 +42,6 @@ class VideoFR:
 
         self.frame_number = 0
 
-        self.encoded_faces = []
-
         self.LAST_UNKNOWN_NUMBER = 0
 
         for key in Dataset.known_faces.keys():
@@ -45,54 +50,78 @@ class VideoFR:
                 self.LAST_UNKNOWN_NUMBER = int((key.split("_")[1])[:-len(VideoFR.EXTENSION)]) + 1
 
     def capture_faces(self, image: list) -> bool:
+        """
+        Capture faces from an image
+        :param image: Image is a list to use the reference and not have a copy
+        :return: Return True if there is a face, else return False
+        """
 
+        # Check if the cooldown is over to avoid too many requests
         if not self.frame_number % VideoFR.COOLDOWN_INTRUDER == 0:
             self.frame_number += 1
             return False
 
         # Load Face data for the actual frame
+        # First we get the face locations
         face_locations = face_recognition.face_locations(image[0])
+        # Then we get the face encodings
         face_encodings = face_recognition.face_encodings(image[0], face_locations)
 
-        if not face_encodings:
-            Logs.info("No face detected")
+        # This code is just here i case there is more than one face in the image, the program is not made for that
+        if len(face_encodings) > 1:
             return False
 
-        pil_image = Image.fromarray(image[0])
-        draw = ImageDraw.Draw(pil_image)
+        # If there is no face, return False
+        if not face_encodings:
+            return False
 
         # Check if there is any match with the known faces if there is a face
+        # top, right, bottom, left will be removed cause we don't need to draw on the image
         for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-            matches = face_recognition.compare_faces(self.encoded_faces, face_encoding)
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(Dataset.known_faces_encoded, face_encoding)
+            # Default name is Unknown
             name = "Unknown Person"
 
             # If there is a match, get the name
             if True in matches:
-                name = get_key_from_value(Dataset.known_faces, self.encoded_faces[matches.index(True)])
+                # Get the DA or the Unknown name
+                name = get_key_from_value(Dataset.known_faces, Dataset.known_faces_encoded[matches.index(True)])
+                # Checking is the function returned something
+                if name is None:
+                    name = "Unknown Person"
+
             else:
+                # If there is no match, save the face and add it to the known faces
+                # I increment the number of the last unknown person
                 self.LAST_UNKNOWN_NUMBER += 1
                 if Dataset.save_face(f"Unknown_{self.LAST_UNKNOWN_NUMBER:03d}", face_encoding):
-                    self.encoded_faces.append(face_encoding)
                     name = f"Unknown_{self.LAST_UNKNOWN_NUMBER:03d}"
 
-            # Draw the rectangle around the face
-            draw.rectangle(((left + 2, top + 2), (right + 2, bottom + 2)), outline=(255, 255, 0))
-            text_width, text_height = draw.textsize(name)
-            draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 0), font=VideoFR.FONT)
+            if self.debug_mode:
+                # Convert the image to PIL format (This code will be removed cause we don't need to draw on the image)
+                pil_image = Image.fromarray(image[0])
+                draw = ImageDraw.Draw(pil_image)
+                # Draw the rectangle around the face
+                # Same I said this code will be removed, only in debug mode
+                draw.rectangle(((left + 2, top + 2), (right + 2, bottom + 2)), outline=(255, 255, 0))
+                text_width, text_height = draw.textsize(name)
+                draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 0), font=VideoFR.FONT)
 
-            # Add the face to the array
-            self.left_face_array.append(left)
-            self.right_face_array.append(right)
-            self.text_height_array.append(text_height)
-            self.bottom_face_array.append(bottom)
+                # Add the face to the array
+                self.left_face_array.append(left)
+                self.right_face_array.append(right)
+                self.text_height_array.append(text_height)
+                self.bottom_face_array.append(bottom)
 
-            self.name_array.append(name)
-        del draw
+                self.name_array.append(name)
 
-        # Convert the image to OpenCV format
-        image[0] = np.array(pil_image)
-        image[0] = cv2.cvtColor(image[0], cv2.COLOR_BGR2RGB)
+                # Convert the image to OpenCV format
+                image[0] = np.array(pil_image)
+                image[0] = cv2.cvtColor(image[0], cv2.COLOR_BGR2RGB)
+                del draw
 
+        # Print the name of the faces
         for face_name in self.name_array:
             Logs.info(f"Codes : {face_name}")
 
@@ -112,32 +141,4 @@ class VideoFR:
         self.text_height_array.clear()
 
 
-# The Video Capture for FR check every 10 frames (1 second) only if there's a face detected
-if __name__ == "__main__":
-    videofr_instance = VideoFR()
 
-    Dataset.load_file()
-
-    videofr_instance.encoded_faces = list(Dataset.known_faces.values())
-
-    # Set camera 0 as default
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, img = cap.read()
-        # Set the img in a list
-        images = [img]
-
-        # Here we use the list just assigned to pass by reference the image and not have to return it
-        if videofr_instance.capture_faces(images):
-            cv2.imshow("Face Recognition", images[0])
-
-        # Reset frame number
-        if videofr_instance.frame_number == 100:
-            videofr_instance.frame_number = 0
-            cv2.destroyAllWindows()
-
-        # Press q to quit
-        if cv2.waitKey(1) == ord('q') & 0xFF:
-            break
-
-    cv2.destroyAllWindows()
